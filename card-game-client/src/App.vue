e441961935392b350801f2168167a2d8482911f7
Oxygen <xjyaitf@gmail.com>
2019-01-03 17:35:39 +0800

    加样式

--- card-game-client/src/App.vue
+++ card-game-client/src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <style>
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    font-family: "Microsoft YaHei", Arial, Helvetica, sans-serif, "宋体";
+    overflow: hidden;
 }
 </style>
--- card-game-client/src/pages/GameTable.vue
+++ card-game-client/src/pages/GameTable.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>{{message}}</div>
+    <div class="app">
+        <div class="table">
+            <div class="other-card-area">
+
+            </div>
+            <div class="my-card-area">
+
+            </div>
+        </div>
+
+        <div class="my-card">
+
+        </div>
+    </div>
 </template>
 
 <script>
@@ -22,3 +35,51 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+    .app {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        user-select: none;
+    }
+
+    .my-card {
+        position: absolute;
+        bottom: 20px;
+        width: 100%;
+        min-height: 170px;
+        display: flex;
+        justify-content: center;
+        background: #f00;
+    }
+
+    .table {
+        width: 100%;
+        height: 100%;
+    }
+
+    .my-card-area {
+        width: 100%;
+        height: 33%;
+        min-height: 170px;
+        position: absolute;
+        bottom: 210px;
+        display: flex;
+        padding: 10px;
+        box-sizing: border-box;
+        justify-content: center;
+        background-color: #0f0;
+    }
+
+    .other-card-area {
+        width: 100%;
+        min-height: 170px;
+        display: flex;
+        padding: 10px;
+        justify-content: center;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        background-color: #00f;
+    }
+</style>
